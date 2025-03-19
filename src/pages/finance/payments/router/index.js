0c1826c7388f994fa536d84477186b6a6bf56407
Jamal Sambo <jhsambo99@gmail.com>
2025-03-19 22:27:09 +0200

    ajustes na routa da intistituicao

--- src/pages/finance/payments/router/index.js
+++ src/pages/finance/payments/router/index.js
@@ -5,8 +5,6 @@ import Enrollment from '../pages/PaymentEnrollment.vue'
 
 
 const paymentsRoutes = [
-   { path: 'institution/create', name: 'create-institution', component: Create },
-   { path: 'institution/:institutionId/edit', name: 'edit-institution', component: Create },
    { path: 'institution/:institutionId/education/:educationId/payment/settings', name: 'settings-payment', component: Settings },
    { path: 'student/:studentId/enrollment/:enrollmentId/payment', name: 'enrollment-payment', component: Enrollment},
 ];
